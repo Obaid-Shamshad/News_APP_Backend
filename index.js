48b79e240178411117d8d2cf297d9e3477bf55d7
Obaid-Shamshad <obaidshamshad5656@gmail.com>
2026-08-12 02:18:50 +0500

    add frotend_url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://news-app-frontend-mu.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
